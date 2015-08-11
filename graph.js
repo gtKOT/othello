d341c654536aa4631231687a50923de1955dc1f4
N.Satoh <nsatoh@azabu-jh.ed.jp>
2015-08-11 22:03:32 +0900

    Graphオブジェクトを作成．
    頂点の集合のみを格納させ，各頂点に，自分を始点とする辺の
    終点と重みとをデータとして持たせる．

---
+++ graph.js
@@ -0,0 +1,50 @@
+(function(nSatohOthello) {
+  'use strict';
+
+  var k;
+  var uniqueId = 0;
+  
+  function Graph(numberOfVertices) {
+    this._vertices = [];
+    for (k = 0; k < numberOfVertices; k++){
+      this._vertices.push(new Vertex()); 
+    }
+  }
+
+  Graph.prototype.getVertices = function() {
+    return this._vertices;
+  };
+  
+  function Vertex() {
+    this._id = uniqueId;
+    uniqueId ++;
+    this._edges = [];
+  }
+
+  Vertex.prototype.addEdge = function(terminalVertex, direction){
+    var newEdge = new Edge(terminalVertex, direction);
+    this._edges.push(newEdge, direction);
+  };
+
+  Vertex.prototype.getEdges = function() {
+    return this._edges;
+  };
+
+  
+  function Edge(terminalVertex, direction) {
+    this._terminalPoint = terminalVertex;
+    this._direction     = direction; // weight of this edge for indicate the direction.
+  }
+
+  Edge.prototype.getTerminal = function() {
+    return this._terminalPoint;
+  };
+
+  Edge.prototype.getDirection = function() {
+    return this._direction;
+  };
+
+ 
+  nSatohOthello.Graph = Graph;
+  
+})(window.nSatohOthello);
