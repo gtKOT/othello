91fefc234575e512484eb70e8d9e0ff29752cdc9
N.Satoh <nsatoh@azabu-jh.ed.jp>
2015-08-22 02:55:07 +0900

    マスのSVG描画前にCellオブジェクトを中間生成する部分を作成．
    メモに書いたことをコーディングし終わったのでメモは削除．
    グラフの各頂点を，idが小さい順に調べて行き，座標を決定する．
    頂点v1からよりidの小さい頂点v2（処理済）への辺がある場合は，
    その辺にそってセルを結合させるように座標決定．
    ない場合は，とりあえず座標は(0, 0)にして，新たなセルグループを作成．
    最小のidを持つ頂点v0とその方向dを記録してqueueにプッシュしておく．
    v0の座標が決定したときに，v1のセルグループに属するセルたちとともに
    v0の座標と-d分だけ移動させ，v1のセルグループをv0のグループに統合する．
    
    コミットメッセージが仕様書になっているという不具合があるが，
    とりあえずこのまま．

--- lattice_renderer.js
+++ lattice_renderer.js
@@ -9,6 +9,7 @@
     this.y = undefined;
     this.vertex = vertex;
     this.group = undefined;
+    this.id = vertex.getId();
   }
   
    
@@ -22,51 +23,94 @@
       </g>
      --*/
 
-    var cells = [];
+    // id: CellObject の形で格納する．現状では配列と大差ない．
+    var cells = {};
 
     // 各頂点の座標決定を待っているグループidを記録
+    // cell_id: [{group_id, direction}, ...] の形
     var suspended_group_queues = {};
 
+    // グループに属しているcellを，id: [cell, ...]の形で格納する
+    var groups = {};
     
+    // 各頂点に対応するセルの座標を決定
+
+    var vertices = graph_data.getVertices();
+    var group_id = 0;
+    for (var v_num = 0; v_num < vertices.length; v_num++) {
+      var v = vertices[v_num];
+      var cell = new Cell(v);
+      cells[cell.id] = cell;
+      
+      var min_edge = v.getMinEdge();
+      var min_terminal_id = min_edge.getTerminal().getId();
+      var direction_vector = cell_directions[min_edge.getDirection()];
+      
+      if (v.getId() > min_terminal_id) {
+        // 既存のセルグループに追加
+        var parent_cell = cells[min_terminal_id];
+        
+        cell.group = parent_cell.group;
+        cell.x = parent_cell.x - direction_vector[0] * cell_width;
+        cell.y = parent_cell.y - direction_vector[1] * cell_height;
+        groups[cell.group].push(cell);
+      } else {
+        // 新規セルグループ
+        cell.group = group_id;
+        group_id ++;
+        cell.x = 0; // 気持ちの上ではここを - direction_vector[0] * cell_width にしたい(というか new Cell(v)の時点でしたい)が
+        cell.y = 0; // initial cellだけ処理を変えないと面倒になりそうなのでそうしない．
+        groups[cell.group] = [cell];
+        if (v_num > 0){ // 最初のvertexはqueueに入れない．
+          // 未定義なら空リスト作成
+          suspended_group_queues[min_terminal_id] = suspended_group_queues[min_terminal_id] || [];
+          suspended_group_queues[min_terminal_id].push({group:cell.group, direction:direction_vector});
+        }
+      }
+
+      // このcell用のqueueにある子グループの各child_cellを，
+      // このcellの位置 - direction 分だけ移動してこのセルグループに結合させる．
+      var queue = suspended_group_queues[cell.id] || []; // undefined回避
+      for (var k = 0; k < queue.length; k++ ) {
+        var child_group_id = queue[k].group;
+        var direction_to_parent = queue[k].direction;
+        var child_group = groups[child_group_id];
+        for (var cell_k = 0; cell_k < child_group.length; cell_k++ ) {
+          var child_cell = child_group[cell_k];
+          child_cell.x += cell.x - direction_to_parent[0] * cell_width;
+          child_cell.y += cell.y - direction_to_parent[1] * cell_height;
+          child_cell.group = cell.group;
+        }
+
+        // 子グループを親グループに統合
+        groups[cell.id] += groups[child_group_id];
+        groups[child_group_id] = null;
+      }                  
+    }
+
+
+
+
     // board 全体を平行移動するためにグループ要素
     var group_of_cells = svg_util.createG({
       id: "cells"
     });
 
-    board_svg.appendChild(board_cells);
-
-    /*--
-      for v in vertices:
-          min_terminal = min(e.getTerminal for e in v.getEdges)
-          if min_terminal in already_rendered:
-              get x, y of vertex (whose id == min_terminal)
-              set (x of v) = x + direction[0] * cell_width 
-              set (y of v) = y + direction[1] * cell_height
-              record group_id 
-          else:
-              set x, y = 0, 0 # temp 
-              push min_terminal to queue
-              group_id ++
-              record group_id
-          search id of v in queue
-          if found:
-              move the group who wait v
-              and rewrite group_id of it
-    --*/
+    board_svg.appendChild(group_of_cells);
 
     
     //-- 下地 --------------------------------------------
-    board_cells.appendChild(svg_util.createRect({
+    group_of_cells.appendChild(svg_util.createRect({
       'class': 'ground',
       x: 0,
       y: 0,
-      width : cell_width  * size,
-      height: cell_height * size
+      width : cell_width,
+      height: cell_height
     }));
    
     //--- 罫線 -------------------------------------------
     for (i = 1; i <= size - 1; i++) {
-      board_cells.appendChild(svg_util.createLine({
+      group_of_cells.appendChild(svg_util.createLine({
         'class': 'rule',
         x1: cell_width * i,
         y1: 0,
@@ -87,7 +131,7 @@
     //--- 4箇所のドット ----------------------------------
     for (i = 0; i < 2; i++) {
       for (j = 0; j < 2; j++) {
-        board_cells.appendChild(svg_util.createCircle({
+        group_of_cells.appendChild(svg_util.createCircle({
           'class': 'dot',
           cx: cell_width  * 2 + cell_width  * half_size * i,
           cy: cell_height * 2 + cell_height * half_size * j,
